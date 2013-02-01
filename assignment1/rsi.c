93913f66d141ce67932884e47c64286147ca76c4
Ryan McDonald <ryan.mcdonald8@gmail.com>
2013-02-01 15:54:09 -0800

    Fully functional with signal handler. Switched to readline method instead of fgets.

--- assignment1/rsi.c
+++ assignment1/rsi.c
@@ -9,17 +9,18 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 
-#define DEBUG_MODE 0
+#define DEBUG_MODE 1
 #define MAX_INPUT_LENGTH 255
 
 void parse_command (char user_command[], char *arguments[]);
 
 /*
 	Questions: 
-	1. Do we only need to implement the ls, mkdir, cd, cat, nohup, ps, and kill commands?
-	2. If not, there's no good way to decide whether or not to use execvp() or chdir() based on their input.
-	3. So we just have to parse the first argument and run a certain function based on if it's one of the above?
-	4. After executing ls using execvp, why doesn't it pring a new line anymore?
+	1. If you use fork() in the middle of a while loop, it creates a new process which basically copies the code from that point down. So the child
+		process will go all the way down to the bottom of the while loop, then will it go back to the top? Even though it didn't copy the top of the
+		while loop?
+	2. Does cd have to run in the background also? That means it would have to be done in the parent process instead of the child process, so we can't
+		use waitpid?
 	
 	TESTS:
 	1. mkdir test
@@ -33,25 +34,61 @@ void parse_command (char user_command[], char *arguments[]);
 	9. ...
 	
 	TODO:
-	1. 
+	1. setbuf(stdin, NULL); // Is this necessary?
 */
+
+/* SIGCHLD handler. A child process returns SIGCHLD when it is stopped or terminated. */
+static void sigchld_handler (int signal)
+{
+	while (waitpid(-1, NULL, WNOHANG) > 0) {
+		if (DEBUG_MODE) printf("\nSIGCHLD handler activated. Received signal %d\n", signal);
+	}
+	
+	if (DEBUG_MODE)	printf("Parent process receives SIGCHLD.\n");
+}
+
 int main()
 {
-	char user_command[MAX_INPUT_LENGTH];
+	//char user_command[MAX_INPUT_LENGTH];
+	char * user_command;
 	pid_t child_pid;
 	int child_status;
 	
+	// The sigaction is used to change the action taken by a process on receipt of a specific signal.
+	struct sigaction action;
+	memset (&action, '\0', sizeof(action));
+	// We want to run the sigchld_handler on receipt of a signal
+    action.sa_handler = sigchld_handler;
+	
+	// Attach our sigchld_handler. The new action for a SIGCHLD signal is installed from action, which is the struct holding our sigchld_handler.
+	if (sigaction(SIGCHLD, &action, 0)) {
+		perror ("sigaction");
+		return 1;
+	}
+	
 	int inputting = 1;
 	while (inputting) {
-		printf("RSI: %s > ", getcwd(0, 0));
+		//printf("RSI: %s > ", getcwd(0, 0));
+		//fflush(stdin);
+		//setbuf(stdin, NULL);
+		
+		
 		
 		// TAKE INPUT
-		fgets(user_command, sizeof(user_command), stdin);
-		// Get rid of the trailing new line character
-		if (user_command[strlen(user_command) - 1] == '\n') {
-			  user_command[strlen(user_command) - 1] = '\0';
-		}
 		
+		char * prompt = malloc(snprintf(NULL, 0, "RSI: %s > ", getcwd(0, 0)) + 1);
+		sprintf(prompt, "RSI: %s > ", getcwd(0, 0));
+		
+		//fgets(user_command, sizeof(user_command), stdin);
+		user_command = readline(prompt);
+		
+		//fgetc(stdin);
+			
+		// Get rid of the trailing new line character
+		//if (user_command[strlen(user_command) - 1] == '\n') {
+		//	  user_command[strlen(user_command) - 1] = '\0';
+		//}		
+
 		if (DEBUG_MODE)	printf("Command: %s\n", user_command);
 		// END OF TAKE INPUT
 
@@ -60,6 +97,7 @@ int main()
 		char **arguments = NULL;
 		int number_of_arguments = 0;
 		int i;
+		int background_process = 0;
 
 		// Use strtok to grab first token. This is the command to execute.
 		char *argument = strtok(user_command, " ");
@@ -77,9 +115,20 @@ int main()
 			argument = strtok(NULL, " ");
 		}
 
-		// The array needs to be null-terminated for execvp
-		arguments = realloc (arguments, sizeof (char*) * (number_of_arguments + 1));
-		arguments[number_of_arguments] = NULL;
+		// Check if they want to run the process in the background
+		if (strcmp(arguments[number_of_arguments - 1], "&") == 0) {
+			background_process = 1;
+			
+			if (DEBUG_MODE) printf("They want to run the process in the background!\n");
+			
+			// We don't need the extra & anymore. null-terminate the array for execvp
+			arguments[number_of_arguments - 1] = NULL;
+			number_of_arguments --;
+		} else {
+			// The array needs to be null-terminated for execvp
+			arguments = realloc (arguments, sizeof (char*) * (number_of_arguments + 1));
+			arguments[number_of_arguments] = NULL;
+		}
 
 		if (DEBUG_MODE) {
 			for (i = 0; i < (number_of_arguments + 1); i++)
@@ -88,7 +137,7 @@ int main()
 		// END OF PARSE INPUT
 		
 		// EXECUTE INPUT
-		if (strcmp(arguments[0], "cd") == 0) {
+		if (number_of_arguments >= 1 && strcmp(arguments[0], "cd") == 0) {
 		
 			// Make sure they only specified two arguments, the "cd" and the directory name (or blank).
 			if (number_of_arguments > 2) {
@@ -121,7 +170,7 @@ int main()
 			if (child_pid >= 0) {
 				// Fork returns 0 to the child process
 				if (child_pid == 0) {
-					if (DEBUG_MODE) printf("Child: PID of Child = %ld\n", (long) getpid());
+					if (DEBUG_MODE) printf("CHILD: PID of Child = %ld\n", (long) getpid());
 
 					if (execvp(arguments[0], arguments) < 0) {
 						printf("%s: command not found\n", arguments[0]);
@@ -129,33 +178,39 @@ int main()
 					
 				// Fork returns a new pid to the parent process
 				} else {
-					int opts = WNOHANG | WUNTRACED | WCONTINUED;
+					// Block if we don't want to run in the background
+					int opts = 0;
+					if (background_process) {
+						// Don't block if we want to run in background
+						opts = WNOHANG;
+					}
 					int retVal;
-					if (DEBUG_MODE) printf("Parent: PID of Parent = %ld and PID of its child = %ld\n", (long) getpid(), (long) child_pid);
+					
+					if (DEBUG_MODE) printf("PARENT: PID of Parent = %ld and PID of its child = %ld\n", (long) getpid(), (long) child_pid);
 					
 					// Wait for the child to exit
-					// retVal = waitpid(child_pid, &child_status, opts);
+					retVal = waitpid(child_pid, &child_status, opts);
+				
+					if (DEBUG_MODE) printf("PARENT: Return value of waitpid: %d\n", retVal);
+					//if (DEBUG_MODE) printf("PARENT: Child Status: %d\n", child_status);
 					
-					// if (DEBUG_MODE) printf("Return value of waitpid: %d\n", retVal);
-					// if (DEBUG_MODE) printf("Child Status: %d\n", child_status);
+					if (retVal == -1) { 
+						perror("Fail at waitpid"); 
+						exit(EXIT_FAILURE);
+					}
 					
-					// if (retVal == -1) { 
-						// perror("Fail at waitpid"); 
-						// exit(EXIT_FAILURE);
-					// }
+					//wait(NULL);
 					
 					//Macros below can be found by "$ man 2 waitpid"
-					// if (WIFEXITED(child_status)) {
-						// printf("The child terminated normally, status code=%d\n", WEXITSTATUS(child_status));  // Display the status code of child process
-					// } else if (WIFSIGNALED(child_status)) {
-						// printf("The child was killed by signal %d\n", WTERMSIG(child_status));
-					// } else if (WIFSTOPPED(child_status)) {
-						// printf("The child was stopped by delivery of signal %d\n", WSTOPSIG(child_status));
-					// } else if (WIFCONTINUED(child_status)) {    
-						// printf("The child was resumed by delivery of SIGCONT.\n");   
-					// }
-					
-					wait(NULL);
+					 if (WIFEXITED(child_status)) {
+						printf("The child terminated normally, status code=%d\n", WEXITSTATUS(child_status));  // Display the status code of child process
+					} else if (WIFSIGNALED(child_status)) {
+						printf("The child was killed by signal %d\n", WTERMSIG(child_status));
+					} else if (WIFSTOPPED(child_status)) {
+						printf("The child was stopped by delivery of signal %d\n", WSTOPSIG(child_status));
+					} else if (WIFCONTINUED(child_status)) {    
+						printf("The child was resumed by delivery of SIGCONT.\n");   
+					}
 				}
 				
 			// Fork returns -1 on failure.
@@ -166,8 +221,10 @@ int main()
 
 		}
 		
-		// We are done with the arguments array
+		// We are done with the arguments array and the current input on stdin.
+		//setbuf(stdin, NULL); // Is this necessary?
 		free(arguments);
+		//free(prompt);
 		// END OF EXECUTE INPUT
 	}
 	
